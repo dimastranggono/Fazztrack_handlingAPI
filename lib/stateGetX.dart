d41223e9f544e408cd96c373d6e8fced6fd64c03
dimastranggono <56153501+dimastranggono@users.noreply.github.com>
2022-12-14 14:54:47 +0700

    Done
    Handling API using DIO

---
+++ lib/stateGetX.dart
@@ -0,0 +1,9 @@
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+class stateGetX extends GetxController{
+  var inputUserGetX="";
+
+  handleInputUser(String value){
+    inputUserGetX = value;
+  }
+}
